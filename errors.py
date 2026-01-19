8b49817fad038e841bcf61e0e72bd118c2c51661
tovernaar123 <ben.schellevis@outlook.com>
2026-01-19 16:55:22 +0100

    commmite

--- errors.py
+++ errors.py
@@ -55,54 +55,59 @@ def error_pendulum_fast(x0, v0, T, fv, N, Scheme, M=2000, ref_factor=5, seed=0):
     dv_strong = vc - vr
     dx_weak = np.mean(xc) - np.mean(xr)
     dv_weak = np.mean(vc) - np.mean(vr)
-    return float(np.sqrt(dx_strong*dx_strong + dv_strong*dv_strong)), float(np.mean(np.sqrt(dx_weak*dx_weak + dv_weak*dv_weak)))
+    return float(np.mean(np.sqrt(dx_strong*dx_strong + dv_strong*dv_strong))), float(np.sqrt(dx_weak*dx_weak + dv_weak*dv_weak))
 
-def convergence_plot_strong():
+
+def CreatePlot(scheme,fv,T,fd):
     x0, v0 = 1.0, 0.0
-    T = 2.0
-    a, b, c = 0.3, 0.6, 0.3
-
-    M = 200
-    ref_factor = 7
-    seed = 123
+    M = 2000
+    ref_factor = 4
+    seed = 500
 
     Ns = np.unique(
-        np.round(2**np.linspace(11, 16, 50)).astype(int)
+        np.round(2**np.linspace(10, 15, 50)).astype(int)
     )  
     dts = T / Ns
-
     from joblib import Parallel, delayed
-
     def run_one(N, dt, idx):
-        e = strong_error_pendulum_fast(
-            x0, v0, T, stratfv,
-            N,EulerMaruyamaStep, M=M, ref_factor=ref_factor, seed=seed + idx
+        strong, weak = error_pendulum_fast(
+            x0, v0, T, fv,
+            N,scheme, M=M, ref_factor=ref_factor, seed=seed + idx
         )
-        print(f"N={N:6d}, dt={dt:.3e}, strong_RMS={e:.3e}")
-        return e
-    start = time.time()
+        print(f"N={N:6d}, dt={dt:.3e}, strong={strong:.3e} weak={weak:.3e}")
+        return strong, weak
+    
     errs = Parallel(n_jobs=-1)(
         delayed(run_one)(N, dt, i)
         for i, (N, dt) in enumerate(zip(Ns, dts))
     )
-    end = time.time()
-    print(end - start)
-    print("heelloooooo")
     errs = np.array(errs)
 
-    ref = errs[0] * (dts / dts[0])**0.5
+    errsStrong = errs[:,0]
+    errsWeak = errs[:,1]
+    refStrong = errsStrong[0] * (dts / dts[0])**0.5
+    refweak = errsWeak[0] * (dts / dts[0])**1.0
 
     plt.figure(figsize=(7.5, 5.0))
-    plt.loglog(dts, errs, marker="o", linewidth=2, label="Strong error (RMS)")
-    plt.loglog(dts, ref, linestyle="--", label=r"reference slope $1/2$")
+    plt.loglog(dts, errsStrong, marker="o", linewidth=2, label="Strong error (RMS)")
+    plt.loglog(dts, errsWeak, marker="o", linewidth=2, label="Weak error (RMS)")
+    plt.loglog(dts, refStrong, linestyle="--", label=r"reference slope $1/2$")
+    plt.loglog(dts, refweak, linestyle="--", label=r"reference slope $1$", color='red')
     plt.gca().invert_xaxis()
     plt.grid(True, which="both", alpha=0.3)
     plt.xlabel("dt")
     plt.ylabel("error")
-    plt.title(f"Strong error, M={M}, ref_factor={ref_factor}")
+    # plt.title(title)
     plt.legend()
     plt.tight_layout()
-    plt.savefig("test.png")
+    plt.savefig(fd)
+
+
+
+
+   
 
 if __name__ == "__main__":
-    convergence_plot_strong()
+    CreatePlot(EulerMaruyamaStep,itofv,1.0,"Plot1.png")
+    CreatePlot(EulerMaruyamaStep,stratfv,1.0,"Plot2.png")
+
